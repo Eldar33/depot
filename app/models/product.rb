039053fdae827028993daddd31eff88a31594055
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-15 12:31:43 +1000

    add validates for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # Уникальность
+  validates :title, uniqueness: true
+  # allow_blank если поле пустое, то показывает только эту ошибку, а не все
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
 end
