56a16ba1124e83117b07175db8ca14ddfa98bff1
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-17 16:49:31 +1000

    add test for controller store

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,17 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+
+    # ищется гиперссылки <а> в элементе с id="side", который содержится
+    # в элементе с id="columns", таких элементов должно быть миниум 4 на странице
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', "Programming Ruby 1.9 & 2.0"
+    assert_select '.price', /\$[,\d]+\.\d\d/
+
+    #assert_select если 2ой аргумент число, то метод вернет число,
+    #если строка, то она будет рассматриваться как ожидаемый результат 
+
   end
 
 end
