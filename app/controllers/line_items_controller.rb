b142b880556e0e0309653858da098b06f2fd3dae
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-21 13:12:04 +1000

    add migration combine_items up&down methods

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,9 +35,6 @@ class LineItemsController < ApplicationController
     #@line_item = @cart.line_items.build(product: product)#LineItem.new(line_item_params)
     @line_item = @cart.add_product(product.id)
 
-    # puts "**************"
-    # puts @line_item.inspect
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
