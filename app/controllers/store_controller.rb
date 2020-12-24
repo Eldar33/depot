6efbbb645166f2969a3ba78bc589720e829ef900
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2020-12-24 10:01:09 +1000

    move acrt to left side layout with partial templates

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,10 @@
 class StoreController < ApplicationController
 
   include CounterIndexStore
+  include CurrentCart
 
   before_action :increase_counter, only: [:index]
+  before_action :set_cart, only: [:index]
 
   def index
     @products = Product.order(:title)
