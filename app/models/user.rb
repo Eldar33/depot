0861666576a87edff02ec8a81e07933d31bde37c
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2021-01-28 16:22:09 +1000

    add validates user name, and uncomment gem bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  # Добавлено после генерации scaffold password:digest
+  # для двух полей для паспорта (второе это подтверждение пароля)
   has_secure_password
 end
