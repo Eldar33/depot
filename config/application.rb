cc59a6017ae7f1115ba75779685c6f98dafb4739
Abasov <C:\Users\Abasov\AppData\Roaming\The Bat!>
2021-02-10 13:23:18 +1000

    local for order

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Используем промежуточное ПО, чтобы использовать наши переводы
+    # в javascript, а точнее в React компонентах
+    config.middleware.use I18n::JS::Middleware
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
