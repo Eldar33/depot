0c0b4f1905000aa2e86df81527611aa9f4a84e3f
eldar <eldar_elf_33@mail.ru>
2022-10-27 09:55:30 +1000

    add system test

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -46,4 +46,23 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_text "Order was successfully destroyed"
   end
+
+  test "check routing number" do
+    visit store_index_url
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    # проверяем, что элемента с id = "order_routing_number" нет на форме
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'Pay type'
+
+    # проверяем, что элемент с id = "order_routing_number" появился на форме
+    assert_selector "#order_routing_number"
+  end
 end
