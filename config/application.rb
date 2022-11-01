22d3f61439a2892b1a80cc0d86271530723e29a7
eldar <eldar_elf_33@mail.ru>
2022-11-01 14:49:05 +1000

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,14 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.use I18n::JS::Middleware
+    I18n.locale = :en
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+
+
